a183e92ae3cd1c018ca61846cd0586ae1fc0d5d7
valentinas123 <valentina10solano@gmail.com>
2026-06-21 02:39:23 -0500

    Actualizacion completa de settings y urls para media

--- nnova/urls.py
+++ nnova/urls.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('usuarios.urls')),    # Sistema de autenticación
-    path('app/', include('cursos.urls')),   # Sistema interno de cursos
+    path('', include('usuarios.urls')),  # Sistema de autenticación
+    path('app/', include('cursos.urls')), # Sistema interno de cursos
 ]
 
-# Corrección para que funcione en producción (Railway) con DEBUG = False
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Forzar a Django a servir MEDIA en Railway aunque DEBUG sea False
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
