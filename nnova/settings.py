8a23b3b060e534f02b54b867bf1c8de252d2b170
valentinas123 <valentina10solano@gmail.com>
2026-06-21 01:23:16 -0500

    Agregar origen de confianza para evitar error CSRF 403

--- nnova/settings.py
+++ nnova/settings.py
@@ -149,3 +149,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'valentina10solano@gmail.com'
 EMAIL_HOST_PASSWORD = 'dzlomnwbjullvmpw'
+
+CSRF_TRUSTED_ORIGINS = ['https://railway.app']
