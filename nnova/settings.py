ffc54a678ad9d5996fd3c5e19601517649df7a82
valentinas123 <valentina10solano@gmail.com>
2026-06-21 00:44:51 -0500

    fix settings para railway

--- nnova/settings.py
+++ nnova/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n7(@c@$+&ue8t&6@7jirq5ie)a)ep@d0ema=vjzjfzmc@1+6+v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
