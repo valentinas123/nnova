6ee9348568e1a6975c62d9b1be09d9963b3292fc
valentinas123 <valentina10solano@gmail.com>
2026-06-21 02:41:56 -0500

    Corregir enrutamiento multimedia usando path estandar

--- nnova/urls.py
+++ nnova/urls.py
@@ -3,18 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('usuarios.urls')),  # Sistema de autenticación
     path('app/', include('cursos.urls')), # Sistema interno de cursos
+    
+    # Ruta estándar para servir archivos multimedia en Railway (Producción)
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Forzar a Django a servir MEDIA en Railway aunque DEBUG sea False
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
-else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
